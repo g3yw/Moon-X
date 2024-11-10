156622684e8cab5c3fd59be3c14e1d29c660947f
3mm <77682537+g3yw@users.noreply.github.com>
2024-11-10 07:31:42 +0100

    Delete loader.lua

--- loader.lua
+++
@@ -1,8 +0,0 @@
-local UserInputService = cloneref(game:GetService("UserInputService"))
-local DeviceType = UserInputService.TouchEnabled and "Mobile" or "PC"
-
-if DeviceType == "Mobile" then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/g3yw/Moon-X/refs/heads/main/main-mobile.lua"))()
-elseif DeviceType == "PC" then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/g3yw/Moon-X/refs/heads/main/main.lua"))()
-end
