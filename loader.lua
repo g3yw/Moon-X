0fb71b05020af5e255acbc3b126bcfed790810be
3mm <77682537+g3yw@users.noreply.github.com>
2024-11-10 07:30:26 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -4,5 +4,5 @@ local DeviceType = UserInputService.TouchEnabled and "Mobile" or "PC"
 if DeviceType == "Mobile" then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/g3yw/Moon-X/refs/heads/main/main-mobile.lua"))()
 elseif DeviceType == "PC" then
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/g3yw/Moon-X/refs/heads/main/main.lua")()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/g3yw/Moon-X/refs/heads/main/main.lua"))()
 end
