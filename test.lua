08ec2eb831269df65ca84fd0621f4efc6ddad466
Mateo <159031831+MateoDev2024@users.noreply.github.com>
2025-04-16 14:11:58 +0100

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,53 @@
+return [[
+        if not game:IsLoaded() then
+            repeat task.wait() until game:IsLoaded()
+        end
+
+        local Workspace = game:GetService("Workspace")
+        local TeleportService = game:GetService("TeleportService")
+        local Players = game:GetService("Players")
+        local HttpService = game:GetService("HttpService")
+
+        local function FindNewServer()
+            local Servers = game:HttpGet("https://games.roblox.com/v1/games/" .. game.PlaceId .. "/servers/Public?sortOrder=Asc&limit=100")
+            Servers = HttpService:JSONDecode(Servers)
+
+            for _, Server in ipairs(Servers.data) do
+                if Server.playing < Server.maxPlayers and Server.id ~= game.JobId then
+                    return Server.id
+                end
+            end
+        end
+
+        if Workspace:FindFirstChild("Rendered") then
+            local Rifts = Workspace:FindFirstChild("Rendered"):FindFirstChild("Rifts")
+
+            if isfile("Moonx/SelectedRifts.txt") then
+                local SelectedRiftsRaw = readfile("Moonx/SelectedRifts.txt")
+
+                local Found = false
+
+                for Line in SelectedRiftsRaw:gmatch("[^\r\n]+") do
+                    if Rifts:FindFirstChild(Line) then
+                        (loadstring or load)(game:HttpGet("https://raw.githubusercontent.com/MateoDev2024/MoonX/refs/heads/main/Loader.lua"))()
+                        Found = true
+                        break
+                    end
+                end
+
+                if Found == false then
+                    local NewServer = FindNewServer()
+                    if NewServer then
+                        queue_on_teleport(loadstring(game:HttpGet(""))())
+                        TeleportService:TeleportToPlaceInstance(game.PlaceId, NewServer, Players.LocalPlayer)
+                    end
+                end
+            else
+                (loadstring or load)(game:HttpGet("https://raw.githubusercontent.com/MateoDev2024/MoonX/refs/heads/main/Loader.lua"))()
+                return
+            end
+        else
+            (loadstring or load)(game:HttpGet("https://raw.githubusercontent.com/MateoDev2024/MoonX/refs/heads/main/Loader.lua"))()
+            return 
+        end
+]]
