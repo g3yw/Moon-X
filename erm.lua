89e974d82b9c0d1109de9e73e07c48f24a839434
Mateo <159031831+MateoDev2024@users.noreply.github.com>
2025-04-16 18:50:07 +0100

    Update erm.lua

--- erm.lua
+++ erm.lua
@@ -1,72 +1,92 @@
 return [[
-    if not game:IsLoaded() then
-        repeat task.wait() until game:IsLoaded()
-    end
+        if not game:IsLoaded() then
+            repeat task.wait() until game:IsLoaded()
+        end
 
-    local Workspace = game:GetService("Workspace")
-    local TeleportService = game:GetService("TeleportService")
-    local Players = game:GetService("Players")
-    local Player = Players.LocalPlayer
+        local Workspace = game:GetService("Workspace")
+        local TeleportService = game:GetService("TeleportService")
+        local Players = game:GetService("Players")
+        local HttpService = game:GetService("HttpService")
 
-    repeat task.wait() until Workspace:FindFirstChild("Rendered") and Workspace.Rendered:FindFirstChild("Rifts")
+        local function GetServerId()
+            local CurrentJob = game.JobId
 
-    local Rifts = Workspace.Rendered.Rifts
+            local Response = request({
+                Url = "http://127.0.0.1:5000/get_new_serv",
+                Method = "POST",
+                Body = '{"current_jobid": "'..CurrentJob..'"}',
+                Headers = {
+                    ['Content-Type'] = 'application/json',
+                }
+            })
 
-    if not isfile("Moonx/SelectedRifts.txt") then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/MateoDev2024/MoonX/refs/heads/main/Loader.lua"))()
-        return
-    end
+            if Response.Body ~= "false" then
+                return Response.Body
+            else
+                return CurrentJob
+            end
+        end
 
-    local SelectedRiftsRaw = readfile("Moonx/SelectedRifts.txt")
+        if Workspace:FindFirstChild("Rendered") then
+            local Rifts = Workspace:FindFirstChild("Rendered"):FindFirstChild("Rifts")
+            
+            if not Rifts then
+                repeat task.wait() until Workspace:FindFirstChild("Rendered"):FindFirstChild("Rifts")
+            end
 
-    if SelectedRiftsRaw == "none" then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/MateoDev2024/MoonX/refs/heads/main/Loader.lua"))()
-        return
-    end
+            Rifts = Workspace:FindFirstChild("Rendered"):FindFirstChild("Rifts")
 
-    local Found = false
+            if isfile("Moonx/SelectedRifts.txt") then
+                local SelectedRiftsRaw = readfile("Moonx/SelectedRifts.txt")
 
-    for Line in SelectedRiftsRaw:gmatch("[^\r\n]+") do
-        local Rift = Rifts:FindFirstChild(Line)
-        if Rift then
-            if string.find(Line, "egg") then
-                local Display = Rift:FindFirstChild("Display")
-                local SurfaceGui = Display and Display:FindFirstChild("SurfaceGui")
-                local Icon = SurfaceGui and SurfaceGui:FindFirstChild("Icon")
-                local LuckLabel = Icon and Icon:FindFirstChild("Luck")
-                local Luck = LuckLabel and LuckLabel.Text
+                if SelectedRiftsRaw == "none" then         
+                    (loadstring or load)(game:HttpGet("https://raw.githubusercontent.com/MateoDev2024/MoonX/refs/heads/main/Loader.lua"))()
+                    return
+                end
 
-                if Line ~= "aura-egg" and Luck == "x25" then
-                    loadstring(game:HttpGet("https://raw.githubusercontent.com/MateoDev2024/MoonX/refs/heads/main/Loader.lua"))()
-                    Found = true
-                    break
+                local Found = false
+
+                for Line in SelectedRiftsRaw:gmatch("[^\r\n]+") do
+                    local Rift = Rifts:FindFirstChild(Line)
+                    if Rift then
+                        if string.find(Line, "egg") then
+                            local Display = Rift:FindFirstChild("Display")
+                            local SurfaceGui = Display and Display:FindFirstChild("SurfaceGui")
+                            local Icon = SurfaceGui and SurfaceGui:FindFirstChild("Icon")
+                            local LuckLabel = Icon and Icon:FindFirstChild("Luck")
+                            local Luck = LuckLabel and LuckLabel.Text
+
+                            if Line ~= "aura-egg" and Luck == "x25" then
+                                loadstring(game:HttpGet("https://raw.githubusercontent.com/MateoDev2024/MoonX/refs/heads/main/Loader.lua"))()
+                                Found = true
+                                break
+                            end
+                        else
+                            print("[MoonX] Matching rift found: " .. Line)
+                            loadstring(game:HttpGet("https://raw.githubusercontent.com/MateoDev2024/MoonX/refs/heads/main/Loader.lua"))()
+                            Found = true
+                            break
+                        end
+                    end
+                end
+
+                local NewServer = GetServerId()
+
+                if Found == false then
+                    if NewServer == game.JobId then
+                        (loadstring or load)(game:HttpGet("https://raw.githubusercontent.com/MateoDev2024/MoonX/refs/heads/main/Loader.lua"))()
+                        return
+                    else
+                        queue_on_teleport(loadstring(game:HttpGet("https://raw.githubusercontent.com/g3yw/Moon-X/refs/heads/main/erm.lua"))())
+                        TeleportService:TeleportToPlaceInstance(game.PlaceId, NewServer, Players.LocalPlayer)
+                    end
                 end
             else
-                loadstring(game:HttpGet("https://raw.githubusercontent.com/MateoDev2024/MoonX/refs/heads/main/Loader.lua"))()
-                Found = true
-                break
+                (loadstring or load)(game:HttpGet("https://raw.githubusercontent.com/MateoDev2024/MoonX/refs/heads/main/Loader.lua"))()
+                return
             end
+        else
+            (loadstring or load)(game:HttpGet("https://raw.githubusercontent.com/MateoDev2024/MoonX/refs/heads/main/Loader.lua"))()
+            return 
         end
-    end
-
-    if not Found then
-        local LastTeleportTime = os.time()
-        local function CanTeleport()
-            return os.time() - LastTeleportTime >= 10
-        end
-
-        if CanTeleport() then
-            local Data = loadstring(game:HttpGet("https://raw.githubusercontent.com/g3yw/Moon-X/refs/heads/main/erm.lua"))()
-            queue_on_teleport(Data)
-            task.wait(5)
-
-            local Success, ErrorMessage = pcall(function()
-                TeleportService:TeleportAsync(game.PlaceId, {Player})
-            end)
-
-            if Success then
-                LastTeleportTime = os.time()
-            end
-        end
-    end
 ]]
